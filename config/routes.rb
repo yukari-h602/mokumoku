b017e41e0168c6b6236a94ef2291b87890f60931
yukari <aphws118211317@outlook.jp>
2023-07-05 21:44:27 +0900

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
-  resources :users, only: %i[new create show]
+  resources :users, only: %i[new create show] do
+    resource :relationships, only: %i[create destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :events do
     collection do
       get :future
